c183e27ac042083d7b454f68f6eac54fb1798d0d
Holly Henske <hcover333@gmail.com>
2021-06-17 16:13:44 -0500

    progress! text is now visible and names are working as expectd

--- script.js
+++ script.js
@@ -5,21 +5,22 @@
 window.addEventListener("load", function() {
    let form = this.document.querySelector("form");
    form.addEventListener("submit",function(event) {
+      event.preventDefault();
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
       
       function showElement() {
-         element = document.querySelector("faultyItems");
-         element.style.visibility = 'visible';
-//attempt at making faulty items visible
+         faultyItems = document.getElementById("faultyItems");
 
+         faultyItems.style.visibility = 'visible';
+      } //end of function that's SUPPOSED to make faultyItems visible, but doesn't
 
       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === ""
       || cargoMass.value === "" ) {
          alert("All fields are required!");
-         event.preventDefault();
+         //event.preventDefault();
       } //end of if statement
       
 //got code below from W3 schools
@@ -37,15 +38,15 @@ window.addEventListener("load", function() {
       
       if (isNaN(fuelLevel.value) ||  isNaN(cargoMass.value)) {
          alert("Fuel Level and Cargo Mass must be numbers");
-         event.preventDefault();
+         //event.preventDefault();
       } // end of if statment
       
 //to do: the div with id faultyItems should be updated if something is not ready for launch.
 //1 -use template literals to updte the li elements pilotStatus and copilot status to include their names
       ////create function for what to put in launchStatus
       showElement();  //calling the function I just wrote? maybe?
-   document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName} is ready for launch`
-   document.getElementById("copilotStatus").innerHTML = `Copilot ${copilotName} is ready for launch`
+   document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName.value} is ready for launch`
+   document.getElementById("copilotStatus").innerHTML = `Copilot ${copilotName.value} is ready for launch`
 
 //2 - if the user submits a fuel level below 10,000 liters, change faultyItems to visible and update fuel status th say "There is not enough fuel for the journey 
 // and the text of the h2 element launchStatus should change to "Shuttle not ready for launch" and color change to red
@@ -57,7 +58,7 @@ window.addEventListener("load", function() {
 
 //} else {
  // it would be ready to launch and fuel level would be ready for launch
-}  //end of if statement
+//}  //end of if statement
 
 //3 -If the user submits a cargo mass that is over 10000 kilogras, change the list to visible
 //update the cargo status stating :there is too mch mass for take oof" and change launchStatus to
