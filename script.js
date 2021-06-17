2a50e9e58dd3d0e27c244d6d37276030f1fc736c
Holly Henske <hcover333@gmail.com>
2021-06-17 16:58:34 -0500

    updating html for shuttle readiness is complete and functional

--- script.js
+++ script.js
@@ -13,9 +13,18 @@ window.addEventListener("load", function() {
       
       function showElement() {
          faultyItems = document.getElementById("faultyItems");
-
          faultyItems.style.visibility = 'visible';
-      } //end of function that's SUPPOSED to make faultyItems visible, but doesn't
+      } //end of function
+
+      function redText() {
+         launchStatus = document.getElementById("launchStatus");
+         launchStatus.style.color = 'red';
+      }
+
+      function greenText() {
+         launchStatus = document.getElementById("launchStatus");
+         launchStatus.style.color = 'green';
+      }
 
       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === ""
       || cargoMass.value === "" ) {
@@ -50,22 +59,31 @@ window.addEventListener("load", function() {
 
 //2 - if the user submits a fuel level below 10,000 liters, change faultyItems to visible and update fuel status th say "There is not enough fuel for the journey 
 // and the text of the h2 element launchStatus should change to "Shuttle not ready for launch" and color change to red
-   if (fuelLevel < 10000){
-      document.getElementById("launchStatus") = "Shuttle not ready for launch"
-      
-   }//end of visibility function...that doesn't work....I don't guess
-   
+if (fuelLevel.value > 10000 && cargoMass.value < 10000) {
+   greenText();
+   document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch"
 
-//} else {
- // it would be ready to launch and fuel level would be ready for launch
+} else if (fuelLevel.value < 10000){
+      redText();
+      document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
+      document.getElementById("fuelStatus").innerHTML = "Fuel level NOT high enough for launch"
+   //end of if statement
+
+ // it would be ready to launch and fuel level would be ready for launch      
 //}  //end of if statement
 
 //3 -If the user submits a cargo mass that is over 10000 kilogras, change the list to visible
 //update the cargo status stating :there is too mch mass for take oof" and change launchStatus to
 //"Shuttle not ready for launch" and color changes to red
 
+} else if (cargoMass.value > 10000) {
+      redText();
+      document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
+      document.getElementById("cargoStatus").innerHTML = "There is too much mass for take off"
+   }
 //4 - If the shuttle is ready to launch, change the text of launchStatus to green and display "Shuttle is ready for launch"
-
+// put step 4 above in the if statement and made the other statements into else ifs.
+   
 
 
 
